b9887a29ddc0395baee8969e464b0169a27b4c04
Yuhei FUJITA <contact@fujita.dev>
2020-09-15 03:13:03 +0900

    fix: テストケース追加

--- __tests__/unit/sample.spec.ts
+++ __tests__/unit/sample.spec.ts
@@ -1,5 +1,11 @@
 import {sum} from '../../src/sample';
 
-test('sample', () => {
-    expect(sum(1, 1)).toEqual(2)
+describe('sample', () => {
+    it('整数', () => {
+        expect(sum(1, 1)).toEqual(2);
+    });
+
+    it('小数', () => {
+        expect(sum(1.5, 2)).toEqual(3.5);
+    })
 })
